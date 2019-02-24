8cfa536c6e0fdc5010c296ea052ef62c6ca6c97f
Blaine Price <1wbprice@gmail.com>
2019-02-23 20:32:57 -0500

    Well, that's ... something

--- src/game.rs
+++ src/game.rs
@@ -30,25 +30,55 @@ fn initialise_camera(world: &mut World) {
         .build();
 }
 
-fn initialize_cursor(world: &mut World) {
+fn initialize_cursor(world: &mut World, sprite_sheet: SpriteSheetHandle) {
     let mut transform = Transform::default();
 
     let y = ARENA_HEIGHT / 2.0;
     transform.set_xyz(CURSOR_WIDTH * 0.5, y, 0.0);
 
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet.clone(),
+        sprite_number: 0
+    };
+
     world.create_entity()
         .with(Cursor::new(Faction::Blue))
+        .with(sprite_render.clone())
         .with(transform)
         .build();
+}
+
+fn load_sprite_sheet(world: &mut World) -> SpriteSheetHandle {
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/rust-wars-spritesheet.png",
+            PngFormat,
+            TextureMetadata::srgb_scale(),
+            (),
+            &texture_storage
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/rust-wars-spritesheet.ron",
+        SpriteSheetFormat,
+        texture_handle,
+        (),
+        &sprite_sheet_store
+    )
 }
 
 impl SimpleState for Game {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        let sprite_sheet_handle = load_sprite_sheet(world);
 
         world.register::<Cursor>();
-
-        initialize_cursor(world);
+        initialize_cursor(world, sprite_sheet_handle);
         initialise_camera(world);
     }
 
