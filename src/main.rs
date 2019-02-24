2367b65c5013d8d279339f74c45ba2724eaba078
Blaine Price <1wbprice@gmail.com>
2019-02-23 21:00:48 -0500

    syntax error somewhere

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use amethyst::renderer::{DisplayConfig, DrawFlat2D, Event, Pipeline,
                          RenderBundle, Stage, VirtualKeyCode};
 use amethyst::utils::application_root_dir;
 use amethyst::core::transform::TransformBundle;
+use amethyst::input::InputBundle;
 
-
+mod systems;
 mod game;
 use crate::game::Game;
 
 fn main() -> amethyst::Result<()> {
-
     amethyst::start_logger(Default::default());
+
+    let binding_path = format!("{}/resources/bindings_config.ron", application_root_dir());
+    let input_bundle = InputBundle::<String, String>::new()
+        .with_bindings_from_file(binding_path)?;
 
     let path = format!("{}/resources/display_config.ron", application_root_dir());
     let config = DisplayConfig::load(&path);
@@ -29,7 +33,9 @@ fn main() -> amethyst::Result<()> {
             RenderBundle::new(pipe, Some(config))
                 .with_sprite_sheet_processor()
         )?
-        .with_bundle(TransformBundle::new())?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::CursorSystem, "cursor_system", &["input_system"]);
 
     let mut game = Application::new("./", Game, game_data)?;
 
--- src/systems/cursor.rs
+++ src/systems/cursor.rs
@@ -1,15 +1,35 @@
 use amethyst::core::Transform;
 use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
-use amethyst::input::InputHandler
+use amethyst::input::InputHandler;
+
+use crate::game::{Cursor, Faction, ARENA_HEIGHT, ARENA_WIDTH};
 
 pub struct CursorSystem;
 
 impl<'s> System<'s> for CursorSystem {
     type SystemData = (
         WriteStorage<'s, Transform>,
-        ReadStorage<'s, Game>,
+        ReadStorage<'s, Cursor>,
         Read<'s, InputHandler<String, String>> 
-    );
+    ); 
 
+    fn run(&mut self, (mut transforms, cursors, input): Self::SystemData) {
+        for (cursor, transform) in (&cursors, &mut transforms).join() {
+            let movement = match cursor.faction {
+                Faction::Blue => input.axis_value("vertical"),
+                Faction::Red => input.axis_value("horizontal")
+            };
+
+            if let Some(mv_amount) = movement {
+                if mv_amount != 0.0 {
+                    let faction_name = match cursor.faction {
+                        Faction::Blue => "blue",
+                        Faction::Red => "red"
+                    };
+                    println!("Side {:?} moving {}", faction_name, mv_amount);
+                }
+            }
+        }
+    }
     
 }
