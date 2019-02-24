e5da0594f2f91c01f926338289b1011b7270335b
Blaine Price <1wbprice@gmail.com>
2019-02-23 21:59:22 -0500

    something like this

--- src/systems/cursor.rs
+++ src/systems/cursor.rs
@@ -16,16 +16,24 @@ impl<'s> System<'s> for CursorSystem {
     fn run(&mut self, (mut transforms, cursors, input): Self::SystemData) {
         for (cursor, transform) in (&cursors, &mut transforms).join() {
 
-            let movement = match cursor.faction {
-                Movement::Vertical => input.axis_value("vertical"),
-                Movement::Horizontal => input.axis_value("horizontal")
-            };
+            let vertical_movement = input.axis_value("vertical");
+            let horizontal_movement = input.axis_value("horizontal");
 
-            if let Some(mv_amount) = movement {
+            if let Some(mv_amount) = vertical_movement {
                 let scaled_amount = 1.2 * mv_amount as f32;
                 let paddle_y = transform.translation().y;
                 transform.set_y(
                     (paddle_y + scaled_amount)
+                        .min(ARENA_HEIGHT - CURSOR_HEIGHT * 0.5)
+                        .max(CURSOR_HEIGHT * 0.5),
+                );
+            }
+
+            if let Some(mv_amount) = horizontal_movement {
+                let scaled_amount = 1.2 * mv_amount as f32;
+                let paddle_x = transform.translation().x;
+                transform.set_x(
+                    (paddle_x + scaled_amount)
                         .min(ARENA_HEIGHT - CURSOR_HEIGHT * 0.5)
                         .max(CURSOR_HEIGHT * 0.5),
                 );
