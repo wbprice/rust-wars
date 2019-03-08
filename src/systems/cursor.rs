b5adf46415a794aaca67fb41ccf2c605808c2d04
Blaine Price <1wbprice@gmail.com>
2019-03-08 11:26:10 -0500

    ehhhh

--- src/systems/cursor.rs
+++ src/systems/cursor.rs
@@ -14,28 +14,28 @@ impl<'s> System<'s> for CursorSystem {
     );
 
     fn run(&mut self, (mut transforms, cursors, input): Self::SystemData) {
-        for (cursor, transform) in (&cursors, &mut transforms).join() {
+        for (cursor, transform) in (&mut cursors, &mut transforms).join() {
 
             let vertical_movement = input.axis_value("vertical");
             let horizontal_movement = input.axis_value("horizontal");
 
             if let Some(mv_amount) = vertical_movement {
-                if vertical_movement > 0 {
-                    cursor.y += 1;
+                if mv_amount > 0.0 {
+                    cursor.y += 1.0;
                 } else {
-                    cursor.y -= 1;
+                    cursor.y -= 1.0;
                 }
             }
 
             if let Some(mv_amount) = horizontal_movement {
-                if horizontal_movement > 0 {
-                    cursor.x += 1;
+                if mv_amount > 0.0 {
+                    cursor.x += 1.0;
                 } else {
-                    cursor.x -= 1;
+                    cursor.x -= 1.0;
                 }
             }
 
-            set_position(&cursor, &transform);
+            set_position(&cursor, transform);
         }
     }
 
